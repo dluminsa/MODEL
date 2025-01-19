03955dc55408efe03626d64e0f7813a0089d71fd
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-19 22:34:47 +0300

    Update UPDATES.py

--- pages/UPDATES.py
+++ pages/UPDATES.py
@@ -1,41 +1,30 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.title("Get User's Location")
-
-# Embed JavaScript to get location
-components.html("""
-    <script>
-        // Function to get the user's location
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        const latitude = position.coords.latitude;
-                        const longitude = position.coords.longitude;
-
-                        // Pass the data back to Streamlit
-                        const data = {latitude: latitude, longitude: longitude};
-                        window.parent.postMessage(JSON.stringify(data), "*");
-                    },
-                    (error) => {
-                        console.error(error);
-                    }
-                );
-            } else {
-                console.log("Geolocation is not supported by this browser.");
+# Button to trigger location retrieval
+if st.button('Get My Location'):
+    # Embed JavaScript to get location when the button is clicked
+    components.html("""
+        <script>
+            function getLocation() {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        function(position) {
+                            const latitude = position.coords.latitude;
+                            const longitude = position.coords.longitude;
+                            // Display the coordinates in the Streamlit app
+                            alert("Latitude: " + latitude + "\nLongitude: " + longitude);
+                        },
+                        function(error) {
+                            alert("Error: " + error.message);
+                        }
+                    );
+                } else {
+                    alert("Geolocation is not supported by this browser.");
+                }
             }
-        }
-        getLocation();
-    </script>
-""", height=0)
-
-# Get location data from query params
-location_data = st.query_params.get("location")
-if location_data:
-    # Assuming location_data is a JSON string that contains the coordinates
-    location = eval(location_data[0])  # Convert string to dictionary
-    st.write("Latitude:", location["latitude"])
-    st.write("Longitude:", location["longitude"])
+            getLocation();
+        </script>
+    """, height=0)
 else:
-    st.write("Waiting for location data...")
+    st.write("Click the button to get your location.")
