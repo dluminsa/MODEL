3675510b01de5e6946e4c52cad88d94061b05d51
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-02-18 06:45:29 +0300

    Update UPDATES.py

--- pages/UPDATES.py
+++ pages/UPDATES.py
@@ -139,8 +139,8 @@ elif check == 'MAKE UPDATES':
     partners = dftest.iloc[0,7]
     if partners > 0:
          #partners = int(0)
-         st.write(f'**Client with ART NO {art} had {partners} ellicited**')
-         st.write(f'**OF THESE {partners}, how many have been:**')
+         st.write(f'**Client with ART NO {art} had {partners:,.0f} ellicited**')
+         st.write(f'**OF THESE {partners:,.0f}, how many have been:**')
          col1,col2 = st.columns(2)
          notif = col1.number_input('**NOTIFIED**', value=None, step=1)
          if not notif:
