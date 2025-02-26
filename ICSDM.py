cefaa7238614c38fc07d096fcce6ed359469320a
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-02-26 06:18:59 +0300

    Update ICSDM.py

--- ICSDM.py
+++ ICSDM.py
@@ -854,8 +854,9 @@ else:
 col1, col2, col3 = st.columns(3)
 submit = col3.button('**SUBMIT**')
 dob = str(dob)
+todi = str(tod)
 row1 = [cluster, district, facility,art, results, dob, age, sex, pm, dist, vil, cords, IAC, adher,adh, htn,dm,AS, MH]
-row2 = [facility,art,socialx, econx, healthx, psychx, spirx, otherissue, act, prevx, condoms, vmmc, econix,vl, reason, name, name2]
+row2 = [facility,art,socialx, econx, healthx, psychx, spirx, otherissue, act, prevx, condoms, vmmc, econix,vl, reason, name, name2, todi]
 row3 = [facility,art,cd, vist, lam, tblam, crag, partners, ellig, chid, tested, pos, linked, post, screened, presumed, picked]
 
 if not submit:
