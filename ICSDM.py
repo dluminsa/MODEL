a1e859c421b45328abab0e8c1a88983c825e32bc
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-10 07:25:52 +0300

    Update ICSDM.py

--- ICSDM.py
+++ ICSDM.py
@@ -16,15 +16,8 @@ from streamlit_gsheets import GSheetsConnection
 from datetime import datetime 
 import datetime as dt
 
-# if 'but' not in st.session_state:
-#         st.session_state.but = False
-# if st.session_state.but:
-# # if st.session_state.sub and st.session_state.but:
-#           st.success('**Form has been downloaded, check your downloads**')
-#           time.sleep(2)
-#           st.markdown("""
-#          <meta http-equiv="refresh" content="0">
-#                """, unsafe_allow_html=True)
+if 'but' not in st.session_state:
+        st.session_state.but = False
 cluster = ''
 pos = ''
 vist = ''
@@ -947,7 +940,7 @@ else:
         if st.session_state.sub:
           st.info('**Download this form before form refreshes**')
           def create_docx():
-                   
+              st.session_state.but = True    
                sp = ''
                barx = socialx + econx + healthx + psychx + spirx + otherissue
                bar = barx
@@ -1010,11 +1003,12 @@ else:
           
           doc_file = create_docx()
              # Provide a download button
-          but = st.download_button(
+          st.download_button(
                  label=f"DOWNLOAD FORM FOR ART NO: {art:,.0f}",
                  data=doc_file,
                  file_name=f"FORM FOR ART NO: {art:,.0f}.docx",
                  mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                 on_click=download_clicked
                     )
           if but:
               st.write('HELLO')
