7307cb6031dc917b0a9cfbe87cacd0db026fbcfe
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-18 23:52:54 +0300

    Update UPDATES.py

--- pages/UPDATES.py
+++ pages/UPDATES.py
@@ -248,6 +248,10 @@ elif check == 'MAKE UPDATES':
                              # else:
                              recent = col1.number_input('**NUMBER WITH RECENT RESULT**', value=None, step=1, key=9)
                              long = col2.number_input('**NUMBER WITH LONGTERM RESULTS**', value=None, step=1, key=10)
+                             check = recent + long
+                             if check > pos:
+                                  st.warning('**TOTAL WITH RECENCY RESULTS IS GREATER THAN POSITIVES**')
+                                  st.stop()
     
 elif check == 'DOWNLOAD FORM':
      st.session_state.form = True
