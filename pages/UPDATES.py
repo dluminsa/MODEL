989d7129f615f53957bcbf27075ff15b79b21792
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-19 06:43:03 +0300

    Update UPDATES.py

--- pages/UPDATES.py
+++ pages/UPDATES.py
@@ -271,10 +271,10 @@ elif check == 'MAKE UPDATES':
          elif updcd4 =='NO':
               pass
          else: 
-              cd4results = st.radio('**CD4 RESULTS**', options = ['<200', '>>200', 'BELOW REFRCE', 'ABOVE REFRCE'], horizontal=True, index=None)
-              if not cd4results or cd4results in ['>>200', 'ABOVE REFRCE']:
+              cd4results = st.radio('**CD4 RESULTS**', options = ['BELOW 200', 'ABOVE 200', 'BELOW REFRCE', 'ABOVE REFRCE'], horizontal=True, index=None)
+              if not cd4results or cd4results in ['ABOVE 200', 'ABOVE REFRCE']:
                    pass
-              elif cd4results in ['<200', 'BELOW REFRCE']:
+              elif cd4results in ['BELOW 200', 'BELOW REFRCE']:
                    tblamdone = st.radio('**WAS TB LAM DONE**', options = ['YES', 'NO'], horizontal=True, index=None)
                    if not tblamdone:
                         st.stop()
