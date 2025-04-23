9cb67d3076fa4474cd4a339762454b3adbc628cd
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-04-23 23:43:30 +0300

    Update DASHBOARD.py

--- pages/DASHBOARD.py
+++ pages/DASHBOARD.py
@@ -92,6 +92,7 @@ if 'txb' not in st.session_state:
          st.write("POOR NETWORK, COULDN'T CONNECT TO DELIVERY DATABASE")
          st.stop()
 dftest = st.session_state.txb.copy()
+st.write(dftest.shape[0])
 
 dftest['FACILITY'] = dftest['FACILITY'].astype(str)
 dftest['DT'] = dftest['DATE'].astype(str)
