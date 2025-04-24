70b08628e99511eb487baff1786dca320fa76712
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-04-24 23:15:14 +0300

    Update DASHBOARD.py

--- pages/DASHBOARD.py
+++ pages/DASHBOARD.py
@@ -343,7 +343,7 @@ with col3:
      bledr = notbled.groupby('USE').size().reset_index()
      bledy = bled.groupby('USE').size().reset_index()
      bledr = bledr.rename(columns ={'USE': word, '0': 'NOT BLED'})
-     bledy = bledy.rename(columns ={'USE': word, '0', 'BLED'})
+     bledy = bledy.rename(columns ={'USE': word, '0': 'BLED'})
      bledr['USE'] = bledr['USE'].astype(str)
      bledy['USE'] = bledy['USE'].astype(str)
      bledr = pd.merge(bledy,bledr, how = 'outer', on = 'USE')
