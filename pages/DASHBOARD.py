bde1aa4247cc099f28c6f602903f85e0b87427a3
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-04-24 10:42:43 +0300

    Update DASHBOARD.py

--- pages/DASHBOARD.py
+++ pages/DASHBOARD.py
@@ -194,7 +194,7 @@ else:
      dfuse2['DISTRICT'] = dfuse2['DISTRICT'].astype(str)
      dfuse3 = dfuse2[dfuse2['DISTRICT'].isin(district)].copy()
 
-facility = st.sidebar.multiselect('**CHOOSE A DISTRICT**', dfuse3['FACILITY'].unique(), key='M')
+facility = st.sidebar.multiselect('**CHOOSE A FACILITY**', dfuse3['FACILITY'].unique(), key='M')
 
 if not facility:
      dfuse4 = dfuse3.copy()
